b29dc14dd953c68e452982321808084530287977
shwetapatil <shweta.pattil@firstcry.in>
2021-03-25 22:59:01 +0530

    update UI

--- curd/src/app/update/update.component.ts
+++ curd/src/app/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
+  contactForm = new FormGroup({
+    Name: new FormControl("", [this.NameValidator.bind(this)]),
+    ContactNo: new FormControl("", [this.ContactValidator.bind(this)]),
+    Email: new FormControl("", [this.EmailValidator.bind(this)])
+  })
 
+  NameValidator(formControl: FormControl) {
+    if (!formControl.value) {
+      return { 'error': 'Required.' }
+    }
+  }
+
+  ContactValidator(formControl: FormControl) {
+    if (!formControl.value) {
+      return { 'error': 'Required.' }
+    }
+
+  }
+
+  EmailValidator(formControl: FormControl) {
+    if (!formControl.value) {
+      return { 'error': 'Required.' }
+    }
+
+  }
   constructor() { }
 
   ngOnInit(): void {
